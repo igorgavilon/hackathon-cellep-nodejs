a338c0db0e2480f2b5e17a2884cbd7c7e8690344
igorgavilon <igorgavilon@gmail.com>
2021-07-29 16:12:08 -0400

    versão 1.0 do projeto

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 // const db = require('./dbConnection')
 
+const sortes = require('./mockup');
+
+const db = require('./dbConnection');
+
 const app = express()
 
 // Configurações do Express
@@ -12,11 +16,40 @@ app.use(express.static('./public'))
 
 // Rotas
 // ---------------------------------------------
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
 
-  res.render('index')
+  var result = await db.query('select * from sortes');
+  
+  const max = result.rows.length;
+  const min = 0;
+
+  function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  const indice = getRandomArbitrary(min, max);
+  
+  const {texto} = result.rows[indice];
+  
+
+  res.render('index', {texto: texto})
 
 });
+
+app.get('/admin', (req, res) => {
+  res.render('admin')
+})
+
+app.post('/admin/salvar-texto', async (req, res) => {
+
+  let {texto} = req.body;
+  
+  await db.query('INSERT INTO sortes (texto) VALUES ($1)', [texto],
+  (err, result) => {
+      res.redirect('/')
+  });
+
+})
 
 // Start Server
 app.listen(process.env.PORT || 3000, () => {
--- dbConnection.js
+++ dbConnection.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg')
+const globalSettings = require('./globalSettings');
 
 const client = new Pool({
-  connectionString: process.env.DATABASE_URL || '...',
+  connectionString: process.env.DATABASE_URL || globalSettings.dbString,
   ssl: {
     rejectUnauthorized: false
   }
